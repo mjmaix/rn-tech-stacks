504f1a29c96c718ac472dbb48bc7533cfbc1c812
MJ Abadilla <mjmaix@gmail.com>
2019-03-17 20:40:25 +0800

    * use tslint:recommended"

--- RNTemplateTypeScript/__tests__/App-test.tsx
+++ RNTemplateTypeScript/__tests__/App-test.tsx
@@ -1,14 +1,10 @@
-/**
- * @format
- */
-
-import 'react-native';
-import React from 'react';
-import App from '../App';
+import React from "react";
+import "react-native";
+import App from "../src/App";
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import reactTestRenderer from "react-test-renderer";
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+it("renders correctly", () => {
+  reactTestRenderer.create(<App />);
 });
