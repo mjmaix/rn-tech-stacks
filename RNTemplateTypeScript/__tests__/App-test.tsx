ecc7c51dbbde95392f5bd66d39459a2fb075b67c
MJ Abadilla <mjmaix@gmail.com>
2019-03-17 20:44:01 +0800

    rename import

--- RNTemplateTypeScript/__tests__/App-test.tsx
+++ RNTemplateTypeScript/__tests__/App-test.tsx
@@ -3,8 +3,8 @@ import "react-native";
 import App from "../src/App";
 
 // Note: test renderer must be required after react-native.
-import reactTestRenderer from "react-test-renderer";
+import renderer from "react-test-renderer";
 
 it("renders correctly", () => {
-  reactTestRenderer.create(<App />);
+  renderer.create(<App />);
 });
